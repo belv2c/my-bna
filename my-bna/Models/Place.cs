951c60b7fc83132b877ca94385e59a67a814483f
Brooke Lewis <belv2c@gmail.com>
2018-06-04 14:29:26 -0500

    added note model

---
+++ my-bna/Models/Place.cs
@@ -0,0 +1,11 @@
+﻿namespace my_bna.Models
+{
+    public class Place
+    {
+        public int Place_Id { get; set; }
+        public string Name { get; set; }
+        public string Types { get; set; }
+        public string Vicinity { get; set; }
+        public string Opening_Hours { get; set; }
+    }
+}
