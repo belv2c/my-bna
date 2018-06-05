7792f12fceb6cde48248b6e046a1220f093c7008
Brooke Lewis <belv2c@gmail.com>
2018-06-04 20:10:57 -0500

    setup connection strings

--- my-bna/Program.cs
+++ my-bna/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using my_bna.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace my_bna
 {
